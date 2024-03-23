2b047ec175abc4c1c3773a96e3d80be320a2e5c0
Maxim Petrov <mail@maximpetrov.net>
2024-03-23 11:16:46 +0400

    Add max_image_width

--- blog/system/themes/max/theme-info.php
+++ blog/system/themes/max/theme-info.php
@@ -1,6 +1,7 @@
 <?php return array (
 
   'display_name' => 'Max',
+  'max_image_width' => '1290',
   'meta_viewport' => 'width=device-width, initial-scale=1',
   'global_styles' => '../css/styles.css',
 
