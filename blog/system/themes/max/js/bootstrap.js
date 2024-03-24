14dfc98b820dd279b65a3a917ba1e4489962ea61
Maxim Petrov <mail@maximpetrov.net>
2024-03-24 12:44:34 +0400

    Test script

--- blog/system/themes/max/js/bootstrap.js
+++ blog/system/themes/max/js/bootstrap.js
@@ -1,16 +1,13 @@
-// Import the Bootstrap bundle
+console.log('works')
+// import "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 //
-// This includes Popper and all of Bootstrap's JS plugins.
-
-import "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
 //
-// Place any custom JS here
+// //
+// // Place any custom JS here
+// //
 //
-
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+// // Create an example popover
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//   .forEach(popover => {
+//     new bootstrap.Popover(popover)
+//   })
